a1cbdedd91f8f7e0075c849889128439644825f6
Joey-Zhang-0A0 <2298667492@qq.com>
2021-12-02 17:47:41 +0800

    1. Add baidu tongji 2. Init gitignore file
    Change-Id: I8ebefdc0646a95a35c0ef9b78a2018dfd22137dc

---
+++ docs/js/baidu-tongji.js
@@ -0,0 +1,10 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c123b381185c587bb0e82f3112d4fc23";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
